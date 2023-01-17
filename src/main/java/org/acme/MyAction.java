9101a475431954e586525a565a3dd8ef75e48b7b
Guillaume Smet <guillaume.smet@gmail.com>
2023-01-17 17:54:54 +0100

    Changes

--- src/main/java/org/acme/MyAction.java
+++ src/main/java/org/acme/MyAction.java
@@ -1,11 +1,23 @@
 package org.acme;
 
+import java.io.IOException;
+
+import org.kohsuke.github.GHEventPayload;
+
 import io.quarkiverse.githubaction.Action;
+import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubapp.event.Issue;
 
 public class MyAction {
 
     @Action
-    void test() {
+    void test(@Issue.Opened GHEventPayload.Issue issuePayload, Commands commands) throws IOException {
         System.out.println("Hello from Quarkus GitHub Action");
+
+        commands.appendJobSummary("# Hi from Quarkus GitHub Action");
+
+        commands.error("There was an error");
+
+        issuePayload.getIssue().comment("Hi from our action");
     }
 }
